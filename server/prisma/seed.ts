a4a364d5563d3fa32013281298eb32c2e5054017
tmz81 <tmz_81@hotmail.com>
2022-11-02 21:32:52 -0300

    create seed 🚀

---
+++ server/prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function seed() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'João Silva',
+      email: 'joão@gmail.com',
+      avatarUrl: 'https://github.com/tmz81.png',
+    }
+  })
+
+  const pools = await prisma.pool.create({
+    data: {
+      title: 'Example João Bolão',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-02T00:15:30.610Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-02T00:15:30.610Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  })
+}
+
+seed()
