16c3f5d2cc481d0acacc152f1c99b6f15d4ba467
tmz81 <tmz_81@hotmail.com>
2022-11-03 11:42:50 -0300

    create route post 🚀

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -11,9 +13,29 @@ async function Start() {
   });
 
   fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count()
+    const count = await prisma.pool.count();
 
     return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333 });
